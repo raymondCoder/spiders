7043fb691b85dc2d8ae3b9d115819cd755a0c375
raymond <htzheng2002@163.com>
2019-06-18 22:33:42 +0800

    #change regular method

--- model/parsers.js
+++ model/parsers.js
@@ -1,18 +1,33 @@
 
 function getSeller(html) {
-    //  let step1 = html.match(/<div\s+id="merchant-info"\s+class="a-section\sa-spacing\-(.*)[\r|\n]+(.*)/ig)
-    let step1 = html.match(/sellerProfileTriggerId\W+[\w|\s]+\W+/ig);
+    let rules = [
+        'sellerProfileTriggerId\\W+[\\w|\\s]+\\W+',
+        '>[\\w|\\s]+<',
+        '[\\w|\\s]+'
+    ]
+    return _ruleSequence(html, rules);
+
+}
+
+
+function getPrice(html) {
+   /* let step1 = html.match(/id="priceblock_ourprice"(.*)[^<]/ig);
     if (!step1 || !Array.isArray(step1) || !step1.length) {
         return false;
     }
-
-    let step2 = step1[0].match(/\>[\w|\s]+\</ig);
+    let step2 = step1[0].match(/>(.*)</)
     if (!step2 || !Array.isArray(step2) || !step2.length) {
         return false;
     }
-    return step2[0].replace(/>|</ig, '');
+    return step2[1].replace(/[a-z]+/ig, '');
+*/
+    let rules = [
+        'id="priceblock_ourprice"(.*)[^<]',
+        '>(.*)<',
+        '[\\d|.|,]+'
+    ]
+    return _ruleSequence(html, rules);
 }
-
 
 function getScore(html) {
     let step1 = html.match(/class="a-icon-alt">[\w|\s|.]+[^<]/ig)
@@ -29,40 +44,52 @@ function getScore(html) {
     return step2[1];
 }
 
-function getPrice(html) {
-    let step1 = html.match(/id="priceblock_ourprice"(.*)[^<]/ig);
-    if (!step1 || !Array.isArray(step1) || !step1.length) {
-        return false;
-    }
-    let step2 = step1[0].match(/>(.*)</)
-    if (!step2 || !Array.isArray(step2) || !step2.length) {
-        return false;
-    }
-    return step2[1].replace(/[a-z]+/ig, '');
-}
 function getTile(html) {
-    let step1 = html.match(/id="productTitle"(.*)[\r|\n]+[\w|\s]+/ig);
-    if (!step1 || !Array.isArray(step1) || !step1.length) {
-        return false;
-    }
-    let step2 = step1[0].replace(/\r|\n/ig, '').match(/>[\w|\s]+/);
-    if (!step2 || !Array.isArray(step2) || !step2.length) {
-        return false;
-    }
-    return step2[0].replace(/>\s+/gi, '');;
+    let rules = [
+        'id="productTitle"(.*)[\\r|\\n]+[\\w|\\s]+',
+        '>[\\w|\\s]+',
+        '\\s[\\w|\\s]+'
+    ]
+    return _ruleSequence(html, rules, ['\n', '']);
 }
-
 
 function getReview(html) {
-    let step1 = html.match(/id="acrCustomerReviewText"(.*)[^<]/ig);
-    if (!step1 || !Array.isArray(step1) || !step1.length) {
+    let rules = [
+        'id="acrCustomerReviewText"(.*)[^<]',
+        '>[\\d|,]+'
+    ]
+    return _ruleSequence(html, rules, ['>', '']);
+}
+
+function _ruleSequence(html, rules, replacePair = []) {
+    let result = true;
+    let i = 0;
+    let total = rules.length;
+    let content = html;
+    while (result && i < total) {
+        let ruleItem = new RegExp(
+            rules[i],
+            "ig"
+        );
+        let handleReseult = ruleItem.exec(content);
+        if (!handleReseult || !Array.isArray(handleReseult) || !handleReseult.length) {
+            result = false;
+        } else {
+            content = handleReseult[0];
+        }
+
+        i++;
+    }
+    if (!result || !content) {
         return false;
     }
-    let step2 = step1[0].match(/>[\d|,]+/ig)
-    if (!step2 || !Array.isArray(step2) || !step2.length) {
-        return false;
+    if (replacePair.length) {
+        content = content.replace(
+            new RegExp(replacePair[0],'ig'),
+            replacePair[1]
+        ).trim()
     }
-    return step2[0].replace('>', '');
+    return content;
 }
 
 module.exports = {
--- model/searchPage.js
+++ model/searchPage.js
@@ -1,7 +1,26 @@
 const proRequest = require('./proRequest');
 const htmlParsers = require('./parsers');
 const fs = require('fs');
-const Option = {
+const head_user_agent = ['Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; rv:11.0) like Gecko)',
+    'Mozilla/5.0 (Windows; U; Windows NT 5.2) Gecko/2008070208 Firefox/3.0.1',
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1) Gecko/20070309 Firefox/2.0.0.3',
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1) Gecko/20070803 Firefox/1.5.0.12',
+    'Opera/9.27 (Windows NT 5.2; U; zh-cn)',
+    'Mozilla/5.0 (Macintosh; PPC Mac OS X; U; en) Opera 8.0',
+    'Opera/8.0 (Macintosh; PPC Mac OS X; U; en)',
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.12) Gecko/20080219 Firefox/2.0.0.12 Navigator/9.0.0.6',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Win64; x64; Trident/4.0)',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.2; .NET4.0C; .NET4.0E)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Maxthon/4.0.6.2000 Chrome/26.0.1410.43 Safari/537.1 ',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.2; .NET4.0C; .NET4.0E; QQBrowser/7.3.9825.400)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0 ',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.92 Safari/537.1 LBBROWSER',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; BIDUBrowser 2.x)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/3.0 Safari/536.11']
+let Option = {
     //'Host': 'www.amazon.com',
     // 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0',
     "User-Agent": 'Webkit',
@@ -16,6 +35,13 @@ const Option = {
     'Cache-Control': 'max-age=0',
     'TE': 'Trailers'*/
 };
+
+
+let num = head_user_agent.length;
+let randN = Math.floor(Math.random() * num)
+let hd = head_user_agent[randN]
+
+Option["User-Agent"] = hd;
 const filtersDefault = ['seller', 'price', 'review', 'title', 'score'];
 function _doRequest(url) {
     return proRequest.doGet(
@@ -45,14 +71,15 @@ function log(html) {
 
 }
 function getProductCotent(url, filters = []) {
-    if (!filters) {
+    if (!filters.length) {
         filters = filtersDefault;
-    }
+    }    
     return proRequest.doGet(
         url,
         Option
     ).then((resp) => {
         let html = resp.body;
+        // console.log(html)
         if (!filters) {
             return html;
         }
@@ -70,7 +97,7 @@ function getProductCotent(url, filters = []) {
         let output = {
             url: url,
             asin: url.split('/').pop()
-        }
+        }        
         data.forEach((element, num) => {
             let key = filters[num];
             output[key] = element;
